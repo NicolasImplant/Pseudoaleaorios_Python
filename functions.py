55ea3286ee0da5d1abefd0f0d3e305a93d956308
Nico <nicolasimplant@gmail.com>
2021-11-25 09:41:50 -0500

    Modificaciones proyecto

--- functions.py
+++ functions.py
@@ -39,10 +39,10 @@ def square_mean(Seed:int, v:list=[], random:dict={}) -> list:
     random[Seed] = [vector(v),vector(v)/(10**4)]        
     return random[Seed]
 
-# Función que recibe la semilla y aplica el algoritmo de productos medios retorna el número aleatorio y la nueva semilla
-# Function that receives the seed and applies the product mean algorithm returns the random number and the new seed    
+# Función que recibe la semilla y aplica el algoritmo de multiplicador constante retorna el número aleatorio y la nueva semilla
+# Function that receives the seed and applies the constant multiplier algorithm returns the random number and the new seed    
 
-def constantMultiplier(mult:int, Seed:int, v:list=[], random:dict={}) ->list:
+def constantMultiplier(mult:int, Seed:int, v:list=[], random:dict={}) -> list:
     try:
         return random[Seed*mult]
     except KeyError:
@@ -58,10 +58,31 @@ def constantMultiplier(mult:int, Seed:int, v:list=[], random:dict={}) ->list:
     random[Seed*mult] = [vector(v), vector(v)/(10**4)]
     return random[Seed*mult]
 
+
+# Función que recibe la semilla y aplica el algoritmo de productos medios retorna el número aleatorio y la nueva semilla
+# Function that receives the seed and applies the product mean algorithm returns the random number and the new seed  
+
+
+def product_mean(Seed1:int,Seed2:int, v:list=[], random:dict={}) -> list:
+    try:
+        return random[Seed1*Seed2]
+    except KeyError:
+        w = [] 
+        v = numero(Seed1*Seed2,w)
+        while len(v) % 2 != 0:
+            v.append(0)
+            v = v[-1::] + v[:-1:]
+        if len(v) == 8:
+            v = v[2:-2]
+        else:
+            v = v[int(8-len(v)):int(-(8-len(v)))]
+    random[Seed1*Seed2] = [vector(v),vector(v)/(10**4)]
+    return random[Seed1*Seed2]    
+
 # Función que genera números primos de cuatro cifras para generar semillas.
 # Function that generates four-digit prime numbers to generate seeds.
 
-def primeNumber(primes:list=[]):
+def primeNumber(primes:list=[]) -> list:
     x = {n:[i for i in range(1,n+1) if n % i == 0] for n in range(2000,6000)}
     for key, value in x.items():
         if len(value) == 2:
